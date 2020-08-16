4f3122aeceddf7b0bf9435ad300b7f2336458cec
Mayur Shinde <mayurshinde60@gmail.com>
2020-08-16 14:56:47 +0000

    fixed few bugs

--- setup-server.sh
+++ setup-server.sh
@@ -13,26 +13,23 @@ echo "Created folder at $android_home"
 
 #Download sdk tools
 echo "Downloading android sdk tools..."
-wget "https://dl.google.com/android/repository/commandlinetools-linux-"$linux_tools"_latest.zip" -o "tools.zip"
+wget "https://dl.google.com/android/repository/commandlinetools-linux-"$linux_tools"_latest.zip" #-o "tools.zip"
 echo "Sdk tools downloaded"
 
 echo "Copying tools inside android"
-unzip tools.zip -d $android_home
-rm tools.zip
+unzip "commandlinetools-linux-"$linux_tools"_latest.zip" -d $android_home
+#rm tools.zip
 echo "SDK tools copied to $android_home"
 
 # Configures sdk to be used anywhere
 echo "Configuring SDK..."
-echo -e "\n\n
-        # Android sdk config
-        \n\n
-        export ANDROID_HOME=$android_home \n
-        export PATH=$PATH:$ANDROID_HOME/tools/bin \n
-        export PATH=$PATH:$ANDROID_HOME/platform-tools" >> $HOME/.bashrc
+echo -e "\n\n# Android sdk config\n\nexport ANDROID_HOME=$android_home \nexport PATH=$PATH:$ANDROID_HOME/tools/bin \nexport PATH=$PATH:$ANDROID_HOME/platform-tools" >> $HOME/.bashrc
 source $HOME/.bashrc
-sudo ln -s $ANDROID_HOME/tools/bin/sdkmanager /usr/bin/sdkmanager
-sdkmanager --sdk_root=$ANDROID_HOME --version
+sudo ln -s $android_home/tools/bin/sdkmanager /usr/bin/sdkmanager
+sdkmanager --sdk_root=$android_home --version
 echo "SDK configured."
 
 echo "Downloading required tools for building apk.."
-sdkmanager –sdk_root=$ANDROID_HOME 'platform-tools' 'platforms;android-30'
+sdkmanager --sdk_root=$android_home "platform-tools" "platforms;android-30"
+#sdkmanager –-sdk_root=$android_home --install "platform-tools" "platforms;android-30"
+echo "Tools downloaded"
